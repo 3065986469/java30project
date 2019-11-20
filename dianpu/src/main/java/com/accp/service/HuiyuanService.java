e5b5a912537d2ba799663384367840346a58b2ed
weierzi <1456379458@qq.com>
2019-11-20 17:03:11 +0800

    姚宇1120

--- dianpu/src/main/java/com/accp/service/HuiyuanService.java
+++ dianpu/src/main/java/com/accp/service/HuiyuanService.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.accp.domain.Vipdengji;
 import com.accp.mapper.VipdengjiMapper;
 
 @Service
+@Transactional
 public class HuiyuanService {
 
 	@Autowired
@@ -18,7 +20,7 @@ public class HuiyuanService {
 	
 	//等级查询
 	public List<Vipdengji> query(){
-		return vipdengjiMapper.selectByExample(null);
+		return vipdengjiMapper.djquery();
 	}
 	
 	//等级增加
@@ -28,11 +30,16 @@ public class HuiyuanService {
 	
 	//按id删除等级
 	public int del(int djid) {
-		return vipdengjiMapper.deleteByPrimaryKey(djid);
+		return vipdengjiMapper.del(djid);
 	}
 	
 	//按id查询等级
-	/*
-	 * public Vipdengji djQueryById(int id) { return vipdengjiMapper.select }
-	 */
+	 public Vipdengji djQueryById(int djid) { 
+		 return vipdengjiMapper.djQueryById(djid);
+	}
+	
+	//修改等级
+	 public int jdupd(Vipdengji vipdengji) {
+		 return vipdengjiMapper.updateByPrimaryKey(vipdengji);
+	 }	 
 }
